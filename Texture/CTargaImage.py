13c339866cb886dfb91ad7f53409f0ba3ae2e620
lgl <luguiliang@gmail.com>
2013-03-21 21:11:37 +0800

    Tga image interface
    opengl sample to show tga image

--- Texture/CTargaImage.py
+++ Texture/CTargaImage.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 import struct
+import copy
 
 class TGATypes:
     TGA_NODATA = 0
@@ -15,10 +16,10 @@ IMAGE_RGB = 0
 IMAGE_RGBA = 1
 IMAGE_LUMINANCE = 2
 IMAGE_DATA_UNSIGNED_BYTE = 0
-BOTTOM_LEFT = 0X00
-BOTTOM_RIGHT = 0X10
-TOP_LEFT = 0X20
-TOP_RIGHT = 0X30
+BOTTOM_LEFT = 0x00
+BOTTOM_RIGHT = 0x10
+TOP_LEFT = 0x20
+TOP_RIGHT = 0x30
 
 class rgba_t:
     r=0
@@ -43,11 +44,27 @@ class tgaheader_t:
     bpp = 0             # uchar
     imageDesc = 0       # uchar
     
+    def printInfo(self):
+        print "tga header:"
+        print "idLength: ", self.idLength
+        print "colorMapType: ", self.colorMapType
+        print "imageTypeCode: ", self.imageTypeCode
+        print "colorMapSped: [",
+        for i in range(5):
+            print self.colorMapSped[i],
+        print "]"
+        print "xOrigin: ", self.xOrigin
+        print "yOrigin: ", self.yOrigin
+        print "width: ", self.width
+        print "height: ", self.height
+        print "bpp: ", self.bpp
+        print "imageDesc: ", self.imageDesc
+    
 class CTargaImage:
     m_colorDepth = 0
     m_imageDataType = 0
     m_imageDataFormat = 0
-    m_pImageData = []
+    m_imageData = []
     m_width = 0
     m_height = 0
     m_imageSize = 0
@@ -57,17 +74,145 @@ class CTargaImage:
     
     def load(self, filename):
         fp = open(filename, 'rb')
-        str = fp.read(14)
+        
+        # read header
+        str = fp.read(18)
         tgaheader = tgaheader_t()
+        tgaheader.idLength, tgaheader.colorMapType, tgaheader.imageTypeCode,\
+        tgaheader.colorMapSped[0], tgaheader.colorMapSped[1], tgaheader.colorMapSped[2],\
+        tgaheader.colorMapSped[3], tgaheader.colorMapSped[4], tgaheader.xOrigin,\
+        tgaheader.yOrigin, tgaheader.width, tgaheader.height, tgaheader.bpp,\
+        tgaheader.imageDesc = struct.unpack('8B4H2B', str)
+        
+#        tgaheader.printInfo()
+        
+        if( tgaheader.imageTypeCode != TGATypes.TGA_RGB and 
+            tgaheader.imageTypeCode != TGATypes.TGA_GRAYSCALE and
+            tgaheader.imageTypeCode != TGATypes.TGA_RGB_RLE and
+            tgaheader.imageTypeCode != TGATypes.TGA_GRAYSCALE_RLE and
+            tgaheader.colorMapType != 0):
+            fp.close()
+            print "failed to read file: ", filename
+            return False
+        
+        # read image data
+        self.m_width = tgaheader.width
+        self.m_height = tgaheader.height
+        color_mode = tgaheader.bpp / 8
+        if(color_mode < 3):
+            fp.close()
+            return False
+        self.m_imageSize = self.m_width * self.m_height * color_mode
+        # skip pass the id if there is one
+        fp.read(tgaheader.idLength)
+        
+        if( tgaheader.imageTypeCode == TGATypes.TGA_RGB or
+            tgaheader.imageTypeCode == TGATypes.TGA_GRAYSCALE):
+            unpackstr = '%dB' %(self.m_imageSize)
+            self.m_imageData = list(struct.unpack(unpackstr, fp.read(self.m_imageSize)))
+        else:   # if this is a RLE compressed image
+            color = rgba_t()
+            while(len(self.m_imageData) < self.m_imageSize):
+                id = ord(fp.read(1))
+                if(id > 128):   # run lenght data
+                    length = id - 127
+                    color.b = ord(fp.read(1))
+                    color.g = ord(fp.read(1))
+                    color.r = ord(fp.read(1))
+                    if(color_mode == 4):
+                        color.a = ord(fp.read(1))
+                    while(length > 0):
+                        self.m_imageData.append(color.b)
+                        self.m_imageData.append(color.g)
+                        self.m_imageData.append(color.r)
+                        if(color_mode == 4):
+                            self.m_imageData.append(color.a)
+                        length -= 1
+                        
+                else:
+                    length = id+1   # the num of non RLE pixels
+                    while(length > 0):
+                        color.b = ord(fp.read(1))
+                        color.g = ord(fp.read(1))
+                        color.r = ord(fp.read(1))
+                        if(color_mode == 4):
+                            color.a = ord(fp.read(1))
+                        self.m_imageData.append(color.b)
+                        self.m_imageData.append(color.g)
+                        self.m_imageData.append(color.r)
+                        if(color_mode == 4):
+                            self.m_imageData.append(color.a)
+                        length -= 1
+                    
+        fp.close()
+        
+        global IMAGE_RGB, IMAGE_RGBA, IMAGE_DATA_UNSIGNED_BYTE, IMAGE_LUMINANCE, TOP_LEFT
+        if(tgaheader.imageTypeCode == TGATypes.TGA_RGB or
+           tgaheader.imageTypeCode == TGATypes.TGA_RGB_RLE):
+            if(3 == color_mode):
+                self.m_imageDataFormat = IMAGE_RGB
+                self.m_imageDataType = IMAGE_DATA_UNSIGNED_BYTE
+                self.m_colorDepth = 24
+            else:
+                self.m_imageDataFormat = IMAGE_RGBA
+                self.m_imageDataType = IMAGE_DATA_UNSIGNED_BYTE
+                self.m_colorDepth = 32
+        elif(tgaheader.imageTypeCode == TGATypes.TGA_GRAYSCALE or
+             tgaheader.imageTypeCode == TGATypes.TGA_GRAYSCALE_RLE):
+            self.m_imageDataFormat = IMAGE_LUMINANCE
+            self.m_imageDataType = IMAGE_DATA_UNSIGNED_BYTE
+            self.m_colorDepth = 8
+        
+        if( (tgaheader.imageDesc & TOP_LEFT) == TOP_LEFT):
+            self.FlipVertical()
+        
+        self.SwapRedBlue()
 
-        tgaheader.idLength, tgaheader.colorMapType, tgaheader.imageTypeCode,
-        tgaheader.colorMapSped[0], tgaheader.colorMapSped[1], tgaheader.colorMapSped[2],
-        tgaheader.colorMapSped[3], tgaheader.colorMapSped[4], tgaheader.xOrigin,
-        tgaheader.yOrigin, tgaheader.width, tgaheader.height, tgaheader.bpp,
-        tgaheader.imageDesc = struct.unpack('4B4H2B', str)
+    def SwapRedBlue(self):
+        swap_method = {32: self.SwapRedBlue32, 24: self.SwapRedBlue24, 8: self.SwapRedBlue8,}
+        try:
+            swap_method[self.m_colorDepth]()
+        except KeyError:
+            pass
     
-a = '1'
-b = '2'
-str = struct.pack("cc", a, b)
-print len(str)
-print TGATypes.TGA_INDEXED
+    def SwapRedBlue32(self):
+        for i in range(0, len(self.m_imageData), 4):
+            tmp = self.m_imageData[i]
+            self.m_imageData[i] = self.m_imageData[i+2]
+            self.m_imageData[i+2] = tmp
+            
+    def SwapRedBlue24(self):
+        for i in range(0, len(self.m_imageData), 3):
+            tmp = self.m_imageData[i]
+            self.m_imageData[i] = self.m_imageData[i+2]
+            self.m_imageData[i+2] = tmp
+            
+    def SwapRedBlue8(self):
+        pass
+    
+    def FlipVertical(self):
+        if(len(self.m_imageData) == 0):
+            return False
+        if(self.m_colorDepth != 32 and self.m_colorDepth != 24):
+            return True
+        
+        line_width = 0;
+        if(self.m_colorDepth == 32):
+            line_width = self.m_width * 4
+        elif(self.m_colorDepth == 24):
+            line_width = self.m_width * 3
+        top = 0
+        bottom = line_width*(self.m_height - 1)
+        
+        # swap all the pixels
+        for i in range(self.m_height / 2):
+            tmp = self.m_imageData[top: top+line_width] 
+            self.m_imageData[top: top+line_width] = self.m_imageData[bottom: bottom+line_width]
+            self.m_imageData[bottom: bottom+line_width] = tmp
+            top += line_width
+            bottom -= line_width
+
+        
+if __name__ == '__main__':
+    ct = CTargaImage()
+    ct.load("opengl_logo_un.tga")
--- Texture/LoadTGA.py
+++ Texture/LoadTGA.py
@@ -5,7 +5,11 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 import glfw
 import time
-    
+from CTargaImage import CTargaImage
+
+m_tga = CTargaImage()   
+m_tga_un = CTargaImage()
+
 def MouseHandler(button, state):
     if(button == glfw.MOUSE_BUTTON_RIGHT):
         exit(0)
@@ -33,7 +37,8 @@ def Reshape(width, height):
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(52.0, float(width)/height, 1.0, 1000.0)
+#    gluPerspective(52.0, float(width)/height, 1.0, 1000.0)
+    glOrtho(0.0, width - 1.0, 0.0, height - 1.0, -1.0, 1.0);
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     
@@ -41,14 +46,16 @@ def Render():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)    
     glLoadIdentity()
     
-    gluLookAt(0.0, 12.0, 0.0, 
-              0.0, 0.0, 0.0, 
-              0.0, 0.0, -1.0)
-
+    global m_tga, m_tga_un
+    glRasterPos2i(250, 400)
+    glDrawPixels(m_tga.m_width, m_tga.m_height, GL_RGB, GL_UNSIGNED_BYTE, m_tga.m_imageData)
+    
+    glRasterPos2i(250, 100)
+    glDrawPixels(m_tga_un.m_width, m_tga_un.m_height, GL_RGB, GL_UNSIGNED_BYTE, m_tga_un.m_imageData)
 
 def init():
-    width = 640
-    height = 640
+    width = 1024
+    height = 768
     glfw.Init()
     glfw.OpenWindow(width, height, 8, 8, 8, 0, 24, 0, glfw.WINDOW)
     glfw.SetWindowTitle("glfw line")
@@ -60,7 +67,10 @@ def init():
     glfw.SetMouseButtonCallback(MouseHandler)
     glfw.SetKeyCallback(KeyboardHandler)
     glfw.SetWindowCloseCallback(WindowCLose)
-
+    
+    global m_tga
+    m_tga.load("opengl_logo.tga")
+    m_tga_un.load("opengl_logo_un.tga")
 
 init()
 while(True):
@@ -68,7 +78,7 @@ while(True):
     glfw.SwapBuffers()
     if( glfw.GetKey(glfw.KEY_ESC) == glfw.GLFW_PRESS ):
         break
-    time.sleep(0.02)
+    time.sleep(0.01)
 
 glfw.Terminate()
 
